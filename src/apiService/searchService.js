ff2b95bc87d1ce4a1bde5d113bb0f03f8cfd0dbe
ndbao <nguyenducbao166@gmail.com>
2022-05-27 18:09:27 +0700

    quên đổi q 😂😂

--- src/apiService/searchService.js
+++ src/apiService/searchService.js
@@ -4,7 +4,7 @@ export const search = async (q, type = 'less') => {
     try {
         const res = await request.get(`users/search`, {
             params: {
-                q: '',
+                q,
                 type,
             },
         });
